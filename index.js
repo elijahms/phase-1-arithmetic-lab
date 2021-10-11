3cfaa2385f081fb3da64a3c23609415485a81d9c
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-11 14:10:54 -0400

    commit tree 534f1133d84f6cc406ccb4aa257b2edce5e2a8b8

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 6.2
+let num2 = 10
+let num3 = 24
+let num4 = 20
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+
+let mod = num3 % num4
+
+let max = Math.max(8, 20)
